4e31e54ab34e71319eba31bab96f34d0f8eaf13e
Ankit Joshi <ankit101joshi@gmail.com>
2022-10-12 21:21:11 -0400

    Updated Welcome message.

--- OrderBot/Session.cs
+++ OrderBot/Session.cs
@@ -24,8 +24,8 @@ namespace OrderBot
             switch (this.nCur)
             {
                 case State.WELCOMING:
-                    aMessages.Add("Welcome to Rich's Shawarama!");
-                    aMessages.Add("What size would you like?");
+                    aMessages.Add("Welcome to GTA Properties!");
+                    aMessages.Add("My name is Alice, How may I help you Today?");
                     this.nCur = State.SIZE;
                     break;
                 case State.SIZE:
